0c303048c5285fb0db1af4d2e4940133b4552025
Matteo Bogo <matteo.bogo@gmail.com>
2018-07-30 03:50:21 +0200

    added a top navbar with two links to static and real-time analysis, added a periodic check for the service availability

---
+++ visualizer/client/src/components/NavBar.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import {config} from "../config/config";
+
+//react-bootstrap
+import {
+    Navbar,
+    NavbarBrand,
+    NavItem,
+    Nav,
+} from 'react-bootstrap'
+
+export default class NavBar extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            status: 'offline',
+        };
+    }
+
+    componentDidMount() {
+        this.interval = setInterval(() => {
+
+            fetch(config.API_URL+'/ping')
+                .then((res) => res.json())
+                .then(res => {
+                    if (res.success === true)
+                        this.setState({ status: 'online' });
+                    else
+                        this.setState({ status: 'offline' });
+                })
+                .catch((err) => {
+                    this.setState({ status: 'offline' });
+                });
+        }, 5000)
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval);
+    }
+
+    render() {
+        return(
+            <Navbar staticTop inverse fluid>
+                <Navbar.Header>
+                    <Navbar.Brand>
+                        <a href="#home">Visualizard</a>
+                    </Navbar.Brand>
+                </Navbar.Header>
+                <Nav>
+                    <NavItem eventKey={1} href="#">
+                        Static Analysis
+                    </NavItem>
+                    <NavItem eventKey={2} href="#">
+                        Real-time Analysis
+                    </NavItem>
+                </Nav>
+                <Navbar.Collapse>
+                    <Navbar.Text pullRight>
+                        Service is {this.state.status}
+                    </Navbar.Text>
+                </Navbar.Collapse>
+            </Navbar>
+        );
+    }
+}
