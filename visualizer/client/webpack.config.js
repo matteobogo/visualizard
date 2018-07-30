564b332a9e063e60d6f9b8088ca21c1eb6cd0011
Matteo Bogo <matteo.bogo@gmail.com>
2018-07-30 03:41:54 +0200

    rewriting of webpack configuration with support for react-bootstrap and images visualization

--- visualizer/client/webpack.config.js
+++ visualizer/client/webpack.config.js
@@ -1,58 +1,49 @@
-const webpack = require('webpack');
+const webpack = require("webpack");
+
+const OpenBrowserPlugin = require('open-browser-webpack-plugin');
 const path = require('path');
+const Dotenv = require('dotenv-webpack');
 
-//const ExtractTextPlugin = require('extract-text-webpack-plugin');
-//const CopyWebpackPlugin = require('copy-webpack-plugin');
-
-//const HtmlWebpackPlugin = require('html-webpack-plugin')
-
-const config = () => {
-
-    return {
-        mode: 'development',
-        context: path.resolve(__dirname, 'src'),
-        entry: ['./app.js', './main.scss'],
-        output: {
-            path: path.resolve(__dirname, 'dist'),
-            filename: 'app.bundle.js',
-            publicPath: '/dist/'
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.js$/,
-                    exclude: [/node_modules/],
-                    use: [{
-                        loader: 'babel-loader',
-                        query: {presets: ['env']},
-                    }],
-                },
-                // {
-                //     test: /\.scss$/,
-                //     use: ExtractTextPlugin.extract([
-                //         'css-loader', 'sass-loader'
-                //     ]),
-                // }
-            ],
-        },
-        devServer: {
-            compress: true,
-            inline: true,
-            port: '4800',
-        },
-        // plugins: [
-        //     new ExtractTextPlugin({
-        //         filename: 'main.bundle.css',
-        //         allChunks: true,
-        //     }),
-        //     new CopyWebpackPlugin([
-        //         {
-        //             from: paths.data,
-        //             to: paths.dist + '/data'
-        //         }
-        //     ])
-        // ],
-    }
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve("dist/assets"),
+        filename: "bundle.js",
+        publicPath: "assets"
+    },
+    plugins: [
+        new OpenBrowserPlugin({url: 'http://localhost:4800'}),
+        new Dotenv()
+    ],
+    devServer: {
+        inline: true,
+        contentBase: './dist',
+        hot: true,  //live-reload
+        port: 4800
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                loader: 'babel-loader',
+            },
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader'
+            },
+            {
+                test: /\.png$/,
+                loader: 'url-loader?limit=100000'
+            },
+            {
+                test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+            },
+            {
+                // test: /\.(ttf|otf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?|(jpg|gif)$/,
+                test: /\.(jpg|png|gif|svg|pdf|ico|ttf|otf|eot)$/,
+                loader: 'file-loader'
+            }]
+    },
 };
-
-module.exports = config;
