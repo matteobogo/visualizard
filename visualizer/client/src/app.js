c213610a7e80ec5b98141fe6453b813a20f8079d
Matteo Bogo <matteo.bogo@gmail.com>
2018-07-31 04:38:19 +0200

    added components: HeatMap Row/Column/Point stats, refactored interval picker: now is the configurator. Now there are two heatmaps: a static heatmap and a real-time heatmap

--- visualizer/client/src/app.js
+++ visualizer/client/src/app.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 
 import NavBar from './components/NavBar';
-import IntervalPicker from './components/IntervalPicker';
-import HeatMapStats from './components/HeatMapStats';
-import StaticHeatMap from './components/StaticHeatMap';
+import StaticHeatMapConfigurator from './components/StaticAnalysis/StaticHeatMapConfigurator';
+import StaticHeatMapStats from './components/StaticAnalysis/StaticHeatMapStats';
+import StaticHeatMap from './components/StaticAnalysis/StaticHeatMap';
+import StaticHeatMapRowStats from './components/StaticAnalysis/StaticHeatMapRowStats';
+import StaticHeatMapColumnStats from './components/StaticAnalysis/StaticHeatMapColumnStats';
+import StaticHeatMapPointStats from './components/StaticAnalysis/StaticHeatMapPointStats';
 
 //react-bootstrap
 import {
@@ -12,26 +15,61 @@ import {
     Col
 } from 'react-bootstrap';
 
+const styles = {
+
+};
+
 class App extends Component {
 
     render() {
         return (
-            <div>
-                <NavBar/>
-                <Grid fluid>
-                    <Row className="show-grid">
-                        <Col md={4}>
-                            <IntervalPicker/>
-                        </Col>
-                        <Col md={8}>
-                            <HeatMapStats/>
-                        </Col>
-                    </Row>
-                    <StaticHeatMap/>
-                </Grid>
-            </div>
+            <Grid fluid>
+                <Row>
+                    <Col>
+                        <NavBar/>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col xs={12} sm={5} lg={4}>
+                        <StaticHeatMapConfigurator/>
+                    </Col>
+                    <Col xs={12} sm={7} lg={8}>
+                        <StaticHeatMapStats/>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col xs={12}>
+                        <StaticHeatMap/>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col xs={12} sm={6} md={4}>
+                        <StaticHeatMapRowStats/>
+                    </Col>
+                    <Col xs={12} sm={6} md={4}>
+                        <StaticHeatMapColumnStats/>
+                    </Col>
+                    <Col xs={12} md={4}>
+                        <StaticHeatMapPointStats/>
+                    </Col>
+                </Row>
+            </Grid>
         );
     }
 }
 
 export default App;
+
+
+{/*<div>*/}
+{/*<NavBar/>*/}
+{/*<div style={{width: "100%", display: "flex"}}>*/}
+{/*<div style={{width: "300px"}}>*/}
+{/*<StaticHeatMapConfigurator/>*/}
+{/*</div>*/}
+{/*<div style={{width: "100%"}}>*/}
+{/*<StaticHeatMapStats/>*/}
+{/*</div>*/}
+{/*</div>*/}
+{/*<StaticHeatMap/>*/}
+{/*</div>*/}
