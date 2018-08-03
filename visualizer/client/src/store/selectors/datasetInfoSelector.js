7f046031172df88eed04478edf4e7af34b0a7dab
Matteo Bogo <matteo.bogo@gmail.com>
2018-08-03 05:25:45 +0200

    added functions in selector for obtaining databases/policies/fields/palettes list from visualizard-api, refactored loading/error

--- visualizer/client/src/store/selectors/datasetInfoSelector.js
+++ visualizer/client/src/store/selectors/datasetInfoSelector.js
@@ -7,23 +7,37 @@
  *
  * Note: Reducers can be combined together in the Root Reducer (e.g. /reducers/index), so we need
  * to call the right sub-reducer, e.g. state.SubReducer.parameters.
+ *
+ * Redux Reselect
+ *
+ * Selectors can compute derived data, allowing Redux to store the minimal possible state.
+ * Selectors are efficient. A selector is not recomputed unless one of its argument changes.
+ * Selectors are composable. They can be used as input to other selectors.
  */
+
 import * as types from '../../store/types/datasetInfoTypes';
 
 export const getItems = (state, itemType) => {
 
-    if (itemType === types._TYPE_DATABASE) {
+    switch (itemType) {
 
-        return state.datasetInfo.databases;
+        case types._TYPE_DATABASE:
+
+            return state.datasetInfo.databases;
+
+        case types._TYPE_POLICY:
+
+            return state.datasetInfo.policies;
+
+        case types._TYPE_FIELD:
+
+            return state.datasetInfo.fields;
+
+        case types._TYPE_PALETTE:
+
+            return state.datasetInfo.palettes;
     }
 };
 
-export const getHasErrored = (state) => {
-
-    return state.datasetInfo.hasErrored;
-};
-
-export const getIsLoading = (state) => {
-
-    return state.datasetInfo.isLoading;
-};
+export const getHasErrored = (state) => state.datasetInfo.hasErrored;
+export const getIsLoading = (state) => state.datasetInfo.isLoading;
