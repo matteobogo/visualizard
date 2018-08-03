7d537e9f70ee01802c4e2195b13da7237201e35b
Matteo Bogo <matteo.bogo@gmail.com>
2018-08-03 05:21:48 +0200

    removed bootstrap grid system

--- visualizer/client/src/app.js
+++ visualizer/client/src/app.js
@@ -1,75 +1,19 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 import NavBar from './components/NavBar';
-import StaticHeatMapConfigurator from './components/StaticAnalysis/StaticHeatMapConfigurator';
-import StaticHeatMapStats from './components/StaticAnalysis/StaticHeatMapStats';
-import StaticHeatMap from './components/StaticAnalysis/StaticHeatMap';
-import StaticHeatMapRowStats from './components/StaticAnalysis/StaticHeatMapRowStats';
-import StaticHeatMapColumnStats from './components/StaticAnalysis/StaticHeatMapColumnStats';
-import StaticHeatMapPointStats from './components/StaticAnalysis/StaticHeatMapPointStats';
-
-//react-bootstrap
-import {
-    Grid,
-    Row,
-    Col
-} from 'react-bootstrap';
-
-const styles = {
-
-};
+import StaticAnalysis from './components/StaticAnalysis/index'
 
 class App extends Component {
 
     render() {
         return (
-            <Grid fluid>
-                <Row>
-                    <Col>
-                        <NavBar/>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col xs={12} sm={5} lg={4}>
-                        <StaticHeatMapConfigurator/>
-                    </Col>
-                    <Col xs={12} sm={7} lg={8}>
-                        <StaticHeatMapStats/>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col xs={12}>
-                        <StaticHeatMap/>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col xs={12} sm={6} md={4}>
-                        <StaticHeatMapRowStats/>
-                    </Col>
-                    <Col xs={12} sm={6} md={4}>
-                        <StaticHeatMapColumnStats/>
-                    </Col>
-                    <Col xs={12} md={4}>
-                        <StaticHeatMapPointStats/>
-                    </Col>
-                </Row>
-            </Grid>
+            <div>
+                <NavBar/>
+                <StaticAnalysis/>
+            </div>
         );
     }
 }
 
 export default App;
-
-
-{/*<div>*/}
-{/*<NavBar/>*/}
-{/*<div style={{width: "100%", display: "flex"}}>*/}
-{/*<div style={{width: "300px"}}>*/}
-{/*<StaticHeatMapConfigurator/>*/}
-{/*</div>*/}
-{/*<div style={{width: "100%"}}>*/}
-{/*<StaticHeatMapStats/>*/}
-{/*</div>*/}
-{/*</div>*/}
-{/*<StaticHeatMap/>*/}
-{/*</div>*/}
