9164b7e00986b8790dff5b255893319d3845151a
Matteo Bogo <matteo.bogo@gmail.com>
2018-08-03 05:23:27 +0200

    added constants for fetching operations (loading/success/error) and item's type (database/policy/field/palette)

---
+++ visualizer/client/src/store/types/datasetInfoTypes.js
@@ -0,0 +1,8 @@
+export const FETCH_ITEMS_LOADING        = 'FETCH_ITEMS_LOADING';
+export const FETCH_ITEMS_SUCCESS        = 'FETCH_ITEMS_SUCCESS';
+export const FETCH_ITEMS_ERROR          = 'FETCH_ITEMS_ERROR';
+
+export const _TYPE_DATABASE = 'DATABASE';
+export const _TYPE_POLICY   = 'POLICY';
+export const _TYPE_FIELD    = 'FIELD';
+export const _TYPE_PALETTE  = 'PALETTE';
