34a8cbf79910d188e0be2f3e980daa6d3f2bd903
Matteo Bogo <matteo.bogo@gmail.com>
2018-08-03 05:32:42 +0200

    added a centralized service for handling visualizard-api requests/responses, added urls for databases/policies/fields/palettes endpoints

---
+++ visualizer/client/src/services/visualizardApiFetcher.js
@@ -0,0 +1,21 @@
+import * as config from '../config/config';
+
+export const _URL_DATABASES = '/influx/databases';
+export const _URL_POLICIES  = (database) => `/influx/policies?dbname=${database}`;
+export const _URL_FIELDS    = (database) => `/influx/fields?dbname=${database}`;
+
+export const _URL_PALETTES  = `/heatmaps/palettes`;
+
+export const fetchResources = (resource_uri) => {
+
+    return fetch(config.API_URL + resource_uri)
+        .then(response => {
+            if (!response.ok) throw Error(response.statusText);
+            return response;
+        })
+        .then(response => response.json())
+        .then(response => {
+            if (response.success === false) throw new Error(response.error);
+            else return response.payload;
+        });
+};
