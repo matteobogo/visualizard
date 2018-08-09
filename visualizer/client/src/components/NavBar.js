6a0011b6417be6a7c49ce73a8f2f2116ea701b65
Matteo Bogo <matteo.bogo@gmail.com>
2018-08-09 14:32:48 +0200

    added redux logic for receiving notifications to show with toasts, added warning notification

--- visualizer/client/src/components/NavBar.js
+++ visualizer/client/src/components/NavBar.js
@@ -13,6 +13,11 @@ import {
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+//redux
+import { connect } from 'react-redux';
+import * as actionTypes from '../store/types/actionTypes';
+import { getNotification } from '../store/selectors/notificationsSelector';
+
 const _TIMEOUT_PING = 5000;
 const _TOAST_SETTINGS = {
 
@@ -20,7 +25,7 @@ const _TOAST_SETTINGS = {
     autoClose: _TIMEOUT_PING
 };
 
-export default class NavBar extends React.Component {
+class NavBar extends React.Component {
 
     constructor(props) {
         super(props);
@@ -31,44 +36,78 @@ export default class NavBar extends React.Component {
     }
 
     componentDidMount() {
-        this.interval = setInterval(() => {
+        // this.interval = setInterval(() => {
+        //
+        //     fetch(config.API_URL+'/ping')
+        //         .then((res) => res.json())
+        //         .then(res => {
+        //             if (this.state.isOnline === false) {
+        //                 this.notifyToast('Great! Server is now online!', 'success');
+        //             }
+        //             if (res.success === true) {
+        //                 this.setState({isOnline: true});
+        //             }
+        //         })
+        //         .catch((err) => {
+        //             if (this.state.isOnline === true) {
+        //                 this.notifyToast('Sorry! Service is offline! Try Later!', 'error');
+        //             }
+        //             this.setState({ isOnline: false });
+        //         });
+        // }, _TIMEOUT_PING)
 
-            fetch(config.API_URL+'/ping')
-                .then((res) => res.json())
-                .then(res => {
-                    if (this.state.isOnline === false) {
-                        this.notifyToast('Great! Server is now online!', 'success');
-                    }
-                    if (res.success === true) {
-                        this.setState({isOnline: true});
-                    }
-                })
-                .catch((err) => {
-                    if (this.state.isOnline === true) {
-                        this.notifyToast('Sorry! Service is offline! Try Later!', 'error');
-                    }
-                    this.setState({ isOnline: false });
-                });
-        }, _TIMEOUT_PING)
+        //websockets
+        //const socket = io.connect('http://localhost:3000/ws');
+        //console.log('a');
+        // socket
+        //     .on("connect", () => {
+        //
+        //         console.log('connected!');
+
+        // socket
+        //     .on('connect', () => {
+        //         socket.emit('computeHeatMap', 'test');
+        //     });
+
+            // .send(
+            //     "computeHeatMap",
+            //     {
+            //         type: 'all',
+            //         dbname: 'google_cluster',
+            //         policy: 'autogen',
+            //         start_interval: '2011-02-01T00:15:00.000Z',
+            //         end_interval: '2011-02-04T13:30:00.000Z',
+            //         field: 'mean_cpu_usage_rate',
+            //         n_measurements: 10,
+            //         period: 300
+            //     });
+            // });
     }
 
-    componentWillUnmount() {
-        clearInterval(this.interval);
-    }
+    // componentWillUnmount() {
+    //     clearInterval(this.interval);
+    // }
 
     notifyToast(message, type) {
 
         switch(type) {
 
-            case 'success':
+            case actionTypes.NOTIFICATION_TYPE_SUCCESS:
                 toast.success(message, {
                     position: _TOAST_SETTINGS.position,
                     autoClose: _TOAST_SETTINGS.autoClose
                 });
                 break;
 
-            case 'error':
+            case actionTypes.NOTIFICATION_TYPE_ERROR:
                 toast.error(message, {
+                    position: _TOAST_SETTINGS.position,
+                    autoClose: _TOAST_SETTINGS.autoClose
+                });
+                break;
+
+            case actionTypes.NOTIFICATION_TYPE_WARNING:
+                toast.warn(message, {
                     position: _TOAST_SETTINGS.position,
                     autoClose: _TOAST_SETTINGS.autoClose
                 });
@@ -77,6 +116,14 @@ export default class NavBar extends React.Component {
     }
 
     render() {
+
+        if (this.props.notification.status !== "") {
+
+            this.notifyToast(
+                this.props.notification.status,
+                this.props.notification.notificationType);
+        }
+
         return(
             <div>
                 <ToastContainer/>
@@ -102,3 +149,13 @@ export default class NavBar extends React.Component {
         );
     }
 }
+
+const mapStateToProps = state => {
+
+    return {
+
+        notification: getNotification(state),
+    }
+};
+
+export default connect(mapStateToProps, null)(NavBar);
