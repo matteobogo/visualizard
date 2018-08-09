222b0c458b9133935571e82c9ecde768f31279d7
Matteo Bogo <matteo.bogo@gmail.com>
2018-08-09 14:43:55 +0200

    added websocketmanager component to main app

--- visualizer/client/src/app.js
+++ visualizer/client/src/app.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
+import WebSocketManager from './containers/HeatMapComputationManager';
 import NavBar from './components/NavBar';
 import StaticAnalysis from './components/StaticAnalysis/index'
 
-class App extends Component {
+export default class App extends Component {
 
     render() {
         return (
             <div>
+                <WebSocketManager/>
                 <NavBar/>
                 <StaticAnalysis/>
             </div>
         );
     }
 }
-
-export default App;
