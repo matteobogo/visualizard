d98f0cf9a8d45b037f34208d92860ce00c15b55c
Matteo Bogo <matteo.bogo@gmail.com>
2018-08-09 14:40:15 +0200

    centralized redux types

---
+++ visualizer/client/src/store/types/actionTypes.js
@@ -0,0 +1,24 @@
+export const FETCH_ITEMS_LOADING        = 'FETCH_ITEMS_LOADING';
+export const FETCH_ITEMS_SUCCESS        = 'FETCH_ITEMS_SUCCESS';
+export const FETCH_ITEMS_ERROR          = 'FETCH_ITEMS_ERROR';
+export const FETCH_ITEMS_RESET          = 'FETCH_ITEMS_RESET';
+
+export const CURRENT_CONFIG_SET_ITEM    = 'CURRENT_CONFIG_SET_ITEM';
+export const CURRENT_CONFIG_RESET       = 'CURRENT_CONFIG_RESET';
+
+export const _TYPE_DATABASE             = 'DATABASE';
+export const _TYPE_POLICY               = 'POLICY';
+export const _TYPE_FIELD                = 'FIELD';
+export const _TYPE_PALETTE              = 'PALETTE';
+export const _TYPE_PERIOD               = 'PERIOD';
+export const _TYPE_FIRST_INTERVAL       = 'FIRST_INTERVAL';
+export const _TYPE_LAST_INTERVAL        = 'LAST_INTERVAL';
+
+export const NOTIFICATION               = 'NOTIFICATION';
+
+export const NOTIFICATION_TYPE_SUCCESS  = 'SUCCESS';
+export const NOTIFICATION_TYPE_ERROR    = 'ERROR';
+export const NOTIFICATION_TYPE_WARNING  = 'WARNING';
+
+export const SEND_COMPUTATION_REQUEST   = 'SEND_COMPUTATION_REQUEST';
+export const COMPUTATION_REQUEST_ACK    = 'COMPUTATION_REQUEST_ACK';
