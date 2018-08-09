e1a5ebd848f2c3d30758e54f141b810c965f32b6
Matteo Bogo <matteo.bogo@gmail.com>
2018-08-09 14:37:28 +0200

    refactored external configutations manager, added app mode

--- visualizer/client/src/config/config.js
+++ visualizer/client/src/config/config.js
@@ -1,6 +1,11 @@
-export const API_PROTOCOL   = process.env.API_PROTOCOL  || "http";
-export const API_HOSTNAME   = process.env.API_HOSTNAME  || "localhost";
-export const API_PORT       = process.env.API_PORT      || 3000;
-export const API_ENDPOINT   = process.env.API_ENDPOINT  || "/api";
+export const config = {
 
-export const API_URL        = API_PROTOCOL + '://' + API_HOSTNAME + ':' + API_PORT + API_ENDPOINT;
+    MODE:               process.env.MODE          || "development",
+
+    API_PROTOCOL:       process.env.API_PROTOCOL  || "http",
+    API_HOSTNAME:       process.env.API_HOSTNAME  || "localhost",
+    API_PORT:           process.env.API_PORT      || 3000,
+    API_ENDPOINT:       process.env.API_ENDPOINT  || "/api",
+};
+
+export const API_URL = config.API_PROTOCOL + '://' + config.API_HOSTNAME + ':' + config.API_PORT + config.API_ENDPOINT;
