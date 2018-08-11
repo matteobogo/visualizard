3c0162bcac88e64fa531c8cc6cb33c6102e41030
Matteo Bogo <matteo.bogo@gmail.com>
2018-08-11 22:11:05 +0200

    added heatmap computation actions constants

--- visualizer/client/src/store/types/actionTypes.js
+++ visualizer/client/src/store/types/actionTypes.js
@@ -1,24 +1,29 @@
-export const FETCH_ITEMS_LOADING        = 'FETCH_ITEMS_LOADING';
-export const FETCH_ITEMS_SUCCESS        = 'FETCH_ITEMS_SUCCESS';
-export const FETCH_ITEMS_ERROR          = 'FETCH_ITEMS_ERROR';
-export const FETCH_ITEMS_RESET          = 'FETCH_ITEMS_RESET';
+export const FETCH_ITEMS_LOADING                = 'FETCH_ITEMS_LOADING';
+export const FETCH_ITEMS_SUCCESS                = 'FETCH_ITEMS_SUCCESS';
+export const FETCH_ITEMS_ERROR                  = 'FETCH_ITEMS_ERROR';
+export const FETCH_ITEMS_RESET                  = 'FETCH_ITEMS_RESET';
 
-export const CURRENT_CONFIG_SET_ITEM    = 'CURRENT_CONFIG_SET_ITEM';
-export const CURRENT_CONFIG_RESET       = 'CURRENT_CONFIG_RESET';
+export const CURRENT_CONFIG_SET_ITEM            = 'CURRENT_CONFIG_SET_ITEM';
+export const CURRENT_CONFIG_RESET               = 'CURRENT_CONFIG_RESET';
 
-export const _TYPE_DATABASE             = 'DATABASE';
-export const _TYPE_POLICY               = 'POLICY';
-export const _TYPE_FIELD                = 'FIELD';
-export const _TYPE_PALETTE              = 'PALETTE';
-export const _TYPE_PERIOD               = 'PERIOD';
-export const _TYPE_FIRST_INTERVAL       = 'FIRST_INTERVAL';
-export const _TYPE_LAST_INTERVAL        = 'LAST_INTERVAL';
+export const _TYPE_DATABASE                     = 'DATABASE';
+export const _TYPE_POLICY                       = 'POLICY';
+export const _TYPE_FIELD                        = 'FIELD';
+export const _TYPE_PALETTE                      = 'PALETTE';
+export const _TYPE_PERIOD                       = 'PERIOD';
+export const _TYPE_INTERVALS                    = 'INTERVALS';
 
-export const NOTIFICATION               = 'NOTIFICATION';
+export const NOTIFICATION                       = 'NOTIFICATION';
 
-export const NOTIFICATION_TYPE_SUCCESS  = 'SUCCESS';
-export const NOTIFICATION_TYPE_ERROR    = 'ERROR';
-export const NOTIFICATION_TYPE_WARNING  = 'WARNING';
+export const NOTIFICATION_TYPE_SUCCESS          = 'SUCCESS';
+export const NOTIFICATION_TYPE_ERROR            = 'ERROR';
+export const NOTIFICATION_TYPE_WARNING          = 'WARNING';
 
-export const SEND_COMPUTATION_REQUEST   = 'SEND_COMPUTATION_REQUEST';
-export const COMPUTATION_REQUEST_ACK    = 'COMPUTATION_REQUEST_ACK';
+export const HEATMAP_COMPUTATION_REQUEST        = 'HEATMAP_COMPUTATION_REQUEST';
+export const HEATMAP_COMPUTATION_ACCEPTED       = 'HEATMAP_COMPUTATION_ACCEPTED';
+export const HEATMAP_COMPUTATION_REJECTED       = 'HEATMAP_COMPUTATION_REJECTED';
+export const HEATMAP_COMPUTATION_ANALYZED       = 'HEATMAP_COMPUTATION_ANALYZED';
+export const HEATMAP_COMPUTATION_IN_PROGRESS    = 'HEATMAP_COMPUTATION_IN_PROGRESS';
+export const HEATMAP_COMPUTATION_UPDATED        = 'HEATMAP_COMPUTATION_UPDATED';
+export const HEATMAP_COMPUTATION_ERRORED        = 'HEATMAP_COMPUTATION_ERRORED';
+export const HEATMAP_COMPUTATION_COMPLETED      = 'HEATMAP_COMPUTATION_COMPLETED';
