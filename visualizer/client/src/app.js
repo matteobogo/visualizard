a53c7aafa111c44ac8eda4dd99a312c210e040ef
Matteo Bogo <matteo.bogo@gmail.com>
2018-08-11 22:13:25 +0200

    websocket manager is now heatmap computation container, all the static heatmap analysis components are moved under it

--- visualizer/client/src/app.js
+++ visualizer/client/src/app.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
-import WebSocketManager from './containers/HeatMapComputationManager';
+import HeatMapComputation from './containers/HeatMapComputation';
 import NavBar from './components/NavBar';
-import StaticAnalysis from './components/StaticAnalysis/index'
 
 export default class App extends Component {
 
     render() {
         return (
             <div>
-                <WebSocketManager/>
                 <NavBar/>
-                <StaticAnalysis/>
+                <HeatMapComputation/>
             </div>
         );
     }
