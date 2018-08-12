9dbed34edd3aca444a818f31a3229bdcf395bab7
Matteo Bogo <matteo.bogo@gmail.com>
2018-08-12 03:27:14 +0200

    code refactoring of StaticHeatMap component

--- visualizer/client/src/components/StaticAnalysis/StaticHeatMap.js
+++ visualizer/client/src/components/StaticAnalysis/StaticHeatMap.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import * as config from '../../config/config';
+import React, { Component } from 'react'
 
 //react-bootstrap
 import { Panel } from 'react-bootstrap';
@@ -10,49 +9,7 @@ const styles = {
 
 export default class StaticHeatMap extends React.Component {
 
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            isLoading: false,
-        };
-    }
-
-    componentDidMount() {
-
-        //change to onClick
-        this.setState({isLoading: true});
-
-        // fetch(config.API_URL+'' +
-        //     '/heatmaps?' +
-        //     'type=sortByMachine&' +
-        //     'dbname=google_cluster&' +
-        //     'policy=autogen&' +
-        //     'interval=2011-02-01T00:15:00.000Z,2011-02-04T13:30:00.000Z&' +
-        //     'fields=mean_cpu_usage_rate&' +
-        //     'n_measurements=100&' +
-        //     'period=300')
-        //     .then(res => {
-        //         return res.text();
-        //     })
-        //     .then((res => {
-        //         console.log(res);   //TEST
-        //         this.setState({
-        //             image: res,
-        //             isLoading: false
-        //         });
-        //     }));
-
-        //TODO rimuovere in favore di redux / websocket?
-    }
-
     render() {
-
-        const { isLoading, image } = this.state;
-
-        if (isLoading) {
-            //loading component
-        }
 
         return(
               <Panel>
@@ -60,7 +17,7 @@ export default class StaticHeatMap extends React.Component {
                       <Panel.Title componentClass="h3">HeatMap</Panel.Title>
                   </Panel.Heading>
                   <Panel.Body>
-                      <img src={image} />
+                      <img/>
                   </Panel.Body>
               </Panel>
         );
