5639720942f8a50f74502d06d1607d694e459262
Matteo Bogo <matteo.bogo@gmail.com>
2018-08-12 03:13:45 +0200

    tmp removed devtools and store configs (prod/dev) (client)

--- visualizer/client/src/containers/DevTools.js
+++ visualizer/client/src/containers/DevTools.js
@@ -1,20 +1,20 @@
-import React from 'react';
-
-import { createDevTools } from 'redux-devtools';
-
-import LogMonitor from 'redux-devtools-log-monitor';
-import DockMonitor from 'redux-devtools-dock-monitor';
-
-const DevTools = createDevTools(
-
-    <DockMonitor
-        defaultIsVisible={false}
-        toggleVisibilityKey='ctrl-h'
-        changePositionKey='ctrl-m'>
-
-        <LogMonitor theme='tomorrow' />
-
-    </DockMonitor>
-);
-
-export default DevTools;
+// import React from 'react';
+//
+// import { createDevTools } from 'redux-devtools';
+//
+// import LogMonitor from 'redux-devtools-log-monitor';
+// import DockMonitor from 'redux-devtools-dock-monitor';
+//
+// const DevTools = createDevTools(
+//
+//     <DockMonitor
+//         defaultIsVisible={false}
+//         toggleVisibilityKey='ctrl-h'
+//         changePositionKey='ctrl-m'>
+//
+//         <LogMonitor theme='tomorrow' />
+//
+//     </DockMonitor>
+// );
+//
+// export default DevTools;
--- visualizer/client/src/store/configureStore.js
+++ visualizer/client/src/store/configureStore.js
@@ -1,10 +1,10 @@
-import { config } from '../config/config';
-
-if (config.APP.mode === 'development') {
-    module.exports = require('./configureStore.prod');
-}
-
-else if (config.APP.mode === 'production') {
-
-    module.exports = require('./configureStore.dev');
-}
+// import { config } from '../config/config';
+//
+// if (config.APP.mode === 'development') {
+//     module.exports = require('./configureStore.prod');
+// }
+//
+// else if (config.APP.mode === 'production') {
+//
+//     module.exports = require('./configureStore.dev');
+// }
