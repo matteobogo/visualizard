4ba5e558b75bf5cd3dfa489f9f1c17d3493aeb20
Matteo Bogo <matteo.bogo@gmail.com>
2018-08-15 13:56:06 +0200

    added uuid to heatmap computation process (assigned after heatmap computation request validation), added handler for checking in progress computation (avoid starting of multiple processes of the same request), added uuid checker handler, added validation of uuid in heatmap anaylsis event, added inprogress checking in validation and analysis events

--- visualizer/server/ws/WebSocketManager.js
+++ visualizer/server/ws/WebSocketManager.js
@@ -1,9 +1,44 @@
 const wsTypes = require('../../commons/WebSocketsEvents');
 const heatMapService = require('../services/HeatMaps');
+const uuidv4 = require('uuid/v4');
 
 exports = module.exports = (io) => {
 
     const clientsMap = new Map();
+    const clientData = {
+        uuid: null,
+        heatMapRequest: null,
+        heatMapAnalysis: null,
+        isProcessing: false,
+    };
+
+    const checkUUID = (uuidRequest, uuidStored, socket, stage) => {
+
+        if (uuidRequest !== uuidStored) {
+
+            console.log(`[${socket.id}] has provided an invalid UUID during a ${stage} request`);
+            socket.emit(wsTypes.HEATMAP_PROCESS_ERROR, 'UUID request mismatch');
+            return false;
+        }
+        else return true;
+    };
+
+    const handleInProgressProcessing = (socket, stage) => {
+
+        console.log(`[${socket.id}] makes a ${stage} request, but another one is in progress`);
+        socket.emit(wsTypes.HEATMAP_PROCESS_ERROR, 'HeatMap request processing in progress');
+    };
+
+    const checkInProgressProcessing = (clientData, type) => {
+
+        if (clientData.isProcessing) {
+            handleInProgressProcessing(socket, type);
+            return true;
+        }
+        else return false;
+    };
+
+    /* WebSocket Management */
 
     const socket = io.of('/ws/heatmap');    //namespace
 
@@ -12,7 +47,7 @@ exports = module.exports = (io) => {
 
             /* Client Connections Management */
             console.info(`Client connected [ID=${socket.id}]`);
-            clientsMap.set(socket, 1);
+            clientsMap.set(socket, clientData);
 
             socket.on("disconnect", () => {
                 clientsMap.delete(socket);
@@ -24,8 +59,17 @@ exports = module.exports = (io) => {
             //initialization + validation
             socket.on(wsTypes.HEATMAP_VALIDATION_START, (request) => {
 
+                let clientData = clientsMap.get(socket);
+
+                //check request processing in progress
+                if (checkInProgressProcessing(clientData, wsTypes.HEATMAP_VALIDATION_START))
+                    return;
+
+                clientData.isProcessing = true;
+                clientsMap.set(socket, clientData);
+
                 console.log(
-                    '------------------------------\n' +
+                    '\n------------------------------\n' +
                     'Received a computation request\n' +
                     '------------------------------\n' +
                     `CLIENT_ID: ${socket.id}       \n` +
@@ -34,8 +78,6 @@ exports = module.exports = (io) => {
 
                 console.log(`${JSON.stringify(request)}`);
                 console.log('------------------------------\n');
-
-                //console.log(typeof request);
 
                 //validation
                 heatMapService
@@ -52,53 +94,81 @@ exports = module.exports = (io) => {
                     })
                     .then(validated => {
 
-                        socket.emit(wsTypes.HEATMAP_VALIDATION_SUCCESS, request, validated);
+                        let uuid = uuidv4();
+
+                        clientData.uuid = uuid;
+                        clientData.heatMapRequest = request;
+                        clientsMap.set(socket, clientData);
+
+                        socket.emit(wsTypes.HEATMAP_VALIDATION_SUCCESS, request, uuid);
+                        console.log(`HeatMap request fetched by [${socket.id}] validated\n UUID assigned [${uuid}]`);
                     })
                     .catch(error => {
 
                         socket.emit(wsTypes.HEATMAP_VALIDATION_FAIL, request, error.message);
+                        console.log(`HeatMap request fetched by [${socket.id}] not valid\n ${error.message}`);
+                    })
+                    .then(() => {  //finally in ES6 proposal
+                        clientData.isProcessing = false;
+                        clientsMap.set(socket, clientData);
                     });
             });
 
-            //start processing
-            socket.on(wsTypes.HEATMAP_ANALYSIS_START, (request) => {
+            //analysis
+            socket.on(wsTypes.HEATMAP_ANALYSIS_START, (uuid) => {
 
-                console.log(
-                    '------------------------------\n' +
-                    'Analysis Start                \n' +
-                    '------------------------------\n' +
-                    `CLIENT_ID: ${socket.id}       \n` +
-                    '------------------------------\n' +
-                    'HeatMap Configuration:        \n');
+                let userData = clientsMap.get(socket);
 
-                console.log(`${JSON.stringify(request)}`);
-                console.log('------------------------------\n');
+                //check uuid (assigned during validation)
+                if (!checkUUID(uuid, userData.uuid, socket, wsTypes.HEATMAP_ANALYSIS_START)) return;
+
+                //check request processing in progress
+                if (userData.isProcessing) {
+                    handleInProgressProcessing(socket, wsTypes.HEATMAP_ANALYSIS_START);
+                    return;
+                }
+                else {
+                    userData.isProcessing = true;
+                    clientsMap.set(socket, userData);
+                }
 
                 //analysis
                 heatMapService
                     .heatMapAnalysis({
-                        database: request.database,
-                        policy: request.policy,
-                        startInterval: request.startInterval,
-                        endInterval: request.endInterval,
-                        fields: request.fields,
-                        nMeasurements: request.nMeasurements,
-                        period: request.period,
-                        palette: request.palette,
+                        database: userData.heatMapRequest.database,
+                        policy: userData.heatMapRequest.policy,
+                        startInterval: userData.heatMapRequest.startInterval,
+                        endInterval: userData.heatMapRequest.endInterval,
+                        fields: userData.heatMapRequest.fields,
+                        nMeasurements: userData.heatMapRequest.nMeasurements,
+                        period: userData.heatMapRequest.period,
+                        palette: userData.heatMapRequest.palette,
                     })
                     .then(analysis => {
 
+                        //save analysis
+                        userData.heatMapAnalysis = analysis;
+
                         socket.emit(wsTypes.HEATMAP_ANALYSIS_SUCCESS, analysis);
+                        console.log(`Analysis started by [${socket.id}] of request [${uuid}] completed`);
                     })
                     .catch(error => {
 
+                        socket.emit(wsTypes.HEATMAP_ANALYSIS_FAIL, error.message);
                         console.log(
-                            `Analysis started by [CLIENT_ID]: ${socket.id} failed\n` +
+                            `Analysis started by [${socket.id}] of request [${uuid}] failed\n` +
                             `Error: \n` +
                             `${error.message}` );
-
-                        socket.emit(wsTypes.HEATMAP_ANALYSIS_FAIL, error.message);
                     })
+                    .then(() => {  //finally in ES6 proposal
+                        userData.isProcessing = false;
+                        clientsMap.set(socket, userData);
+                    });
+            });
+
+            //construction
+            socket.on(wsTypes.HEATMAP_CONSTRUCTION_START, (uuid) => {
+
             });
     });
 };
