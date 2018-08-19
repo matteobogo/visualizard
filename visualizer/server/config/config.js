9bec6b02188231a4eaa0902931480bec2c3a384f
Matteo Bogo <matteo.bogo@gmail.com>
2018-08-19 13:11:11 +0200

    added external configs for mongodb

--- visualizer/server/config/config.js
+++ visualizer/server/config/config.js
@@ -8,21 +8,25 @@ const config = {
         port:   process.env.PORT    || '3000',
     },
     INFLUX: {
-        db_host:        process.env.DB_HOST       || 'localhost',
-        db_port:        process.env.DB_PORT       || '8086',
-        db_name:        process.env.DB_NAME       || 'google_cluster',
-        db_user:        process.env.DB_USER       || 'user',
-        db_password:    process.env.DB_PASSWORD   || 'password',
+        db_host:        process.env.INFLUX_DB_HOST       || 'localhost',
+        db_port:        process.env.INFLUX_DB_PORT       || '8086',
+        db_name:        process.env.INFLUX_DB_NAME       || 'google_cluster',
+        db_user:        process.env.INFLUX_DB_USER       || 'user',
+        db_password:    process.env.INFLUX_DB_PASSWORD   || 'password',
     },
-    // MONGO: {
-    //     db_host:        process.env.MONGO.DB_HOST        || 'localhost',
-    //     db_port:        process.env.MONGO.DB_PORT        || '27017',
-    //     db_name:        process.env.MONGO.DB_NAME        || 'google_cluster',
-    //     db_user:        process.env.MONGO.DB_USER        || 'user',
-    //     db_password:    process.env.MONGO.DB_PASSWORD    || 'password',
-    // },
+    MONGO: {
+        db_host:        process.env.MONGO_DB_HOST        || 'localhost',
+        db_port:        process.env.MONGO_DB_PORT        || '27017',
+        db_name:        process.env.MONGO_DB_NAME        || 'visualizard',
+        db_user:        process.env.MONGO_DB_USER        || 'user',
+        db_password:    process.env.MONGO_DB_PASSWORD    || 'password',
+    },
+    REDIS: {
+        db_host:        process.env.REDIS_DB_HOST        || 'localhost',
+        db_port:        process.env.REDIS_DB_PORT        || '6379'
+    },
     TIMESERIES: {
-        period:         process.env.PERIOD        || 300,
+        period:         process.env.TS_PERIOD        || 300,
     },
 };
 
