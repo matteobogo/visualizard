4d620d600b6e4ffd0abd0d4fa748fc53ef9477d7
Matteo Bogo <matteo.bogo@gmail.com>
2018-08-19 14:08:52 +0200

    added mongoose configuration module

--- visualizer/server/database/mongodb.js
+++ visualizer/server/database/mongodb.js
@@ -1,3 +1,19 @@
-const MongoClient = require('mongodb').MongoClient;
+const config = require('../config/config');
+const mongoose = require('mongoose');
 
-//TODO
+exports.moongoseConfig = () => {
+
+    const mongodbURL = `mongodb://${config.MONGO.db_host}/${config.MONGO.db_name}`;
+    mongoose.connect(mongodbURL)
+        .then(() => {
+            console.log(`${mongodbURL} connected`);
+        })
+        .catch((err) => {
+            console.log(`Failed to connect: ${err.message}`);
+        });
+    mongoose.set('debug', true);
+
+    /* Moongose - Schema registration */
+    require('../models/DatasetAnalysis');
+    require('../models/PointsStatsPerTimestamp')
+};
