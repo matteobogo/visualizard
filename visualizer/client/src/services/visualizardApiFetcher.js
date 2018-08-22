1be581bda548eb2b1e04323019e30870871030e6
Matteo Bogo <matteo.bogo@gmail.com>
2018-08-22 10:16:04 +0200

    changes in the API fetcher URLs: time intervals are fetched separately (now there are an URL for the start interval and one for the end interval), added heatmap types endpoint.

--- visualizer/client/src/services/visualizardApiFetcher.js
+++ visualizer/client/src/services/visualizardApiFetcher.js
@@ -4,9 +4,12 @@ export const _URL_DATABASES         = '/influx/databases';
 export const _URL_POLICIES          = (database) => `/influx/policies?dbname=${database}`;
 export const _URL_FIELDS            = (database) => `/influx/fields?dbname=${database}`;
 export const _URL_PERIODS           = '/influx/periods';
-export const _URL_INTERVALS         = (database, policy, field) =>
-    `/influx/intervals?dbname=${database}&policy=${policy}&field=${field}`;
+export const _URL_FIRST_INTERVAL    = (database, policy, field) =>
+    `/influx/firstInterval?dbname=${database}&policy=${policy}&field=${field}`;
+export const _URL_LAST_INTERVAL     = (database, policy, field) =>
+    `/influx/lastInterval?dbname=${database}&policy=${policy}&field=${field}`;
 
+export const _URL_HEATMAP_TYPES = `/heatmaps/types`;
 export const _URL_PALETTES  = `/heatmaps/palettes`;
 
 export const fetchResources = (resource_uri) => {
