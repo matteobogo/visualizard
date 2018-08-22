3275fc16ab9fc7c94304b9fe15d0386602821968
Matteo Bogo <matteo.bogo@gmail.com>
2018-08-22 12:40:35 +0200

    changed redis dependency: from redis to ioredis (better)

--- visualizer/server/cache/redis.js
+++ visualizer/server/cache/redis.js
@@ -1,6 +1,6 @@
 const config = require('../config/config');
 
-const redisClient = require('redis').createClient;
-const redis = redisClient(config.REDIS.db_port, config.REDIS.db_host);
+const Redis = require('ioredis');
+const redis = new Redis(config.REDIS.db_port, config.REDIS.db_host);
 
 module.exports = redis;
