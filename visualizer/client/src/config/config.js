de3048700c1f006ec0c0e520125b6f3e5cf8f9b4
Matteo Bogo <matteo.bogo@gmail.com>
2018-09-05 09:37:37 +0200

    added tiles api endpoint and tile size in ext.config

--- visualizer/client/src/config/config.js
+++ visualizer/client/src/config/config.js
@@ -1,11 +1,15 @@
 export const config = {
 
-    MODE:               process.env.MODE          || "development",
+    MODE:               process.env.MODE                || "development",
 
-    API_PROTOCOL:       process.env.API_PROTOCOL  || "http",
-    API_HOSTNAME:       process.env.API_HOSTNAME  || "localhost",
-    API_PORT:           process.env.API_PORT      || 3000,
-    API_ENDPOINT:       process.env.API_ENDPOINT  || "/api",
+    API_PROTOCOL:       process.env.API_PROTOCOL        || "http",
+    API_HOSTNAME:       process.env.API_HOSTNAME        || "localhost",
+    API_PORT:           process.env.API_PORT            || 3000,
+    API_ENDPOINT:       process.env.API_ENDPOINT        || "/api",
+    IMAGE_ENDPOINT:     process.env.IMAGE_ENDPOINT      || "/images/TILES",
+
+    TILE_SIZE:          process.env.TILE_SIZE     || 256
 };
 
 export const API_URL = config.API_PROTOCOL + '://' + config.API_HOSTNAME + ':' + config.API_PORT + config.API_ENDPOINT;
+export const TILES_URL = `${config.API_PROTOCOL}://${config.API_HOSTNAME}:${config.API_PORT }${config.IMAGE_ENDPOINT}`;
