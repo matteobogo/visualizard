4cc78d56e6f51ea7e464d3d7b58d55ce4d50eaf9
Matteo Bogo <matteo.bogo@gmail.com>
2018-09-05 10:01:56 +0200

    moved application constants from redux action types in a separated file

---
+++ visualizer/client/src/utils/constants.js
@@ -0,0 +1,24 @@
+export const _TYPE_DATABASES                    = 'databases';
+export const _TYPE_POLICIES                     = 'policies';
+export const _TYPE_FIELDS                       = 'fields';
+export const _TYPE_HEATMAP_TYPES                = 'heatmapTypes';
+export const _TYPE_FIRST_INTERVAL               = 'firstInterval';
+export const _TYPE_LAST_INTERVAL                = 'lastInterval';
+export const _TYPE_COMPUTATION_OPTIONS          = 'computationOptions';
+
+export const _TYPE_SELECTED_DATABASE            = 'selectedDatabase';
+export const _TYPE_SELECTED_POLICY              = 'selectedPolicy';
+export const _TYPE_SELECTED_START_INTERVAL      = 'selectedStartInterval';
+export const _TYPE_SELECTED_END_INTERVAL        = 'selectedEndInterval';
+export const _TYPE_SELECTED_FIELD               = 'selectedField';
+
+export const _TYPE_OPTION_ANALYSIS              = 'optionAnalysis';
+export const _TYPE_OPTION_HEATMAP               = 'optionHeatMap';
+
+export const _ERROR_FETCH_FAILED                = 'ERROR_FETCH_FAILED';
+
+export const NOTIFICATION_TYPE_SUCCESS          = 'SUCCESS';
+export const NOTIFICATION_TYPE_ERROR            = 'ERROR';
+export const NOTIFICATION_TYPE_WARNING          = 'WARNING';
+export const NOTIFICATION_TYPE_INFO             = 'INFO';
+export const NOTIFICATION_DELAY                 = 5000;
