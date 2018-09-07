6bf7f5af91615563f5e4671fb837667d01e63dc7
Matteo Bogo <matteo.bogo@gmail.com>
2018-09-07 09:05:18 +0200

    added test for building and storing tiles

--- visualizer/server/test.js
+++ visualizer/server/test.js
@@ -3,7 +3,12 @@ const config = require('./config/config');
 const mongoose = require('mongoose');
 const redis = require('./cache/redis');
 
-const datasetAnalysisService = require('./services/DatasetAnalysisService');
+const constants = require('./utils/constants');
+
+const datasetAnalysisService = require('./services/AnalysisService');
+const heatMapService = require('./services/HeatMapsService');
+
+const influxdb = require('./database/influxdb');
 
 //tip: passing arrow functions to Mocha is discouraged
 
@@ -31,49 +36,156 @@ describe('Dataset Analysis Service', function() {
     //             policy: "autogen",
     //             startInterval: "2011-02-01T00:15:00.000Z",
     //             endInterval: "2011-02-01T00:30:00.000Z",
-    //             analysisType: datasetAnalysisService._ANALYSIS_TYPES.DATASET_ANALYSIS
+    //             analysisType: datasetAnalysisService._ANALYSIS_TYPES.POINTS_STATS_PER_TIMESTAMP_ANALYSIS
     //         };
     //
     //     it('dataset analysis completed', function() {
     //
     //         datasetAnalysisService
-    //             .getAnalysisCached(params,
-    //                 (error, analysis) => { //error first callback pattern
-    //                     if (error) {
-    //
-    //                         console.log(error);
-    //                     }
-    //                     else {
-    //
-    //                         console.log(analysis);
-    //                     }
-    //                 })
+    //             .getAnalysisCached(params)
+    //             .then(res => {
+    //                 console.log(res);
+    //             })
+    //             .catch(err => {
+    //                 console.log(err);
+    //             })
     //     })
     // })
 
-    describe('Analyze Dataset', function() {
+    // describe('Analyze Dataset', function() {
+    //
+    //     it('Dataset is analyzed', function() {
+    //
+    //         const params = {
+    //             database: 'google_cluster',
+    //             policy: 'autogen',
+    //             startInterval: '2011-02-01T00:15:00.000Z',
+    //             endInterval: '2011-03-01T10:50:00.000Z',  //8192 intervals
+    //             period: 300,
+    //             nMeasurements: 0 // 0 means all the time series
+    //         };
+    //
+    //         datasetAnalysisService
+    //             .analyzeDataset(params)
+    //             .then(function(response) {
+    //
+    //                 expect(response).to.be.a('boolean');
+    //                 expect(response).to.equal(true);
+    //             })
+    //             .catch(function(error) {
+    //                 console.log(error.message);
+    //             });
+    //     });
+    // });
 
-        it('Dataset is analyzed', function() {
+    //test heatmap image fetcher
+    // describe('Testing HeatMap Image Fetcher Service', function() {
+    //
+    //     it('HeatMap 64 base encoding fetched', function() {
+    //
+    //          const params = {
+    //              database: 'google_cluster',
+    //              policy: 'autogen',
+    //              startInterval: '2011-02-01T00:15:00.000Z',
+    //              endInterval: '2011-03-01T10:50:00.000Z',  //8192 intervals
+    //              fields: ["mean_cpu_usage_rate"],
+    //              nMeasurements: 10, //all the time series
+    //              period: 300,
+    //              heatMapType: "sortByMachine",
+    //              palette: 'GRAY',
+    //          };
+    //
+    //          heatMapService
+    //              .heatMapFetcher({ heatMapRequest: params })
+    //              .then(data => {
+    //                  console.log(data);
+    //              })
+    //              .catch(err => {
+    //                  console.log(err);
+    //              })
+    //     });
+    // });
 
-            const params = {
-                database: 'google_cluster',
-                policy: 'autogen',
-                startInterval: '2011-02-01T00:15:00.000Z',
-                endInterval: '2011-03-01T10:50:00.000Z',  //8192 intervals
-                period: 300,
-                nMeasurements: 10 //all the time series
-            };
+    //test build heatmap
+    // describe('Testing HeatMap building service', function() {
+    //
+    //     it('HeatMap Image built', function() {
+    //
+    //          const params = {
+    //              database: 'google_cluster',
+    //              policy: 'autogen',
+    //              startInterval: '2011-02-01T00:15:00.000Z',
+    //              endInterval: '2011-03-01T10:50:00.000Z',  //8192 intervals
+    //              fields: ["mean_cpu_usage_rate"],
+    //              nMeasurements: 0, //all the time series
+    //              period: 300,
+    //              heatMapType: 'SORT_BY_MACHINE',
+    //              palette: 'GRAY',
+    //          };
+    //
+    //          heatMapService.
+    //             heatMapBuildAndStore(
+    //                 params,
+    //                 constants.IMAGE_EXTENSIONS.IMAGE_PNG_EXT
+    //          )
+    //              .then(data => {
+    //                  console.log(data);
+    //              })
+    //              .catch(err => {
+    //                  console.log(err);
+    //              });
+    //     });
+    // });
 
-            datasetAnalysisService
-                .analyzeDataset(params)
-                .then(function(response) {
+    // describe('Testing getPointsBatchByDatabaseByPolicyByMultiNameByStartTimeByEndTime', function() {
+    //
+    //     it('Batch of points fetched', function() {
+    //
+    //          influxdb.fetchPointsFromHttpApi(
+    //              'google_cluster',
+    //              'autogen',
+    //              ['resource_usage_10', 'resource_usage_5'],
+    //              '2011-02-01T00:15:00.000Z',
+    //              '2011-03-01T10:50:00.000Z',
+    //              300,
+    //              ['mean_cpu_usage_rate']
+    //          )
+    //              .then(data => {
+    //                  console.log(data);
+    //              })
+    //              .catch(err => {
+    //                  console.log(err);
+    //              });
+    //     });
+    // });
 
-                    expect(response).to.be.a('boolean');
-                    expect(response).to.equal(true);
-                })
-                .catch(function(error) {
-                    console.log(error.message);
-                });
+    describe('Testing HeatMap Image Tiles Builder', function() {
+
+        it('HeatMap Image Tiles built', function() {
+
+             const request = {
+                 database: 'google_cluster',
+                 policy: 'autogen',
+                 startInterval: '2011-02-01T00:15:00.000Z',
+                 endInterval:   '2011-03-01T10:50:00.000Z',
+                 fields: ["mean_cpu_usage_rate"],
+                 nMeasurements: 0, //all the time series
+                 period: 300,
+                 heatMapType: 'SORT_BY_MACHINE',
+                 palette: 'GRAY',
+             };
+
+             heatMapService.heatMapBuildAndStore(
+                 {
+                     request: request,
+                     imageType: constants.IMAGE_EXTENSIONS.IMAGE_PNG_EXT,
+                     mode: constants.HEATMAPS.MODES.TILES,
+                     tileWidth: 256,
+                     tileHeight: 256,
+                 }
+             )
+                 .then(res => console.log(res))
+                 .catch(err => console.log(err));
         });
     });
 });
