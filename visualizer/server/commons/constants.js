8d27409a75411d16a38a4defdb63aa61f22c36c3
Matteo Bogo <matteo.bogo@gmail.com>
2018-09-13 21:57:02 +0200

    integrated commons constants

---
+++ visualizer/server/commons/constants.js
@@ -0,0 +1,8 @@
+const sharedConstants = {
+
+    ANALYSIS_DATASET: 'Dataset Analysis',
+    ANALYSIS_MEASUREMENTS: 'Measurements Analysis',
+    ANALYSIS_PSPT: 'Points per Timestamp Analysis',
+};
+
+module.exports = sharedConstants;
