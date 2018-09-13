e003d0db4292157fae486f2255492d9838bd266e
Matteo Bogo <matteo.bogo@gmail.com>
2018-09-13 22:23:09 +0200

    added websocket reducer in the root reducer

--- visualizer/client/src/store/reducers/index.js
+++ visualizer/client/src/store/reducers/index.js
@@ -4,9 +4,11 @@ import { combineReducers } from 'redux';
 import { datasetInfo } from './datasetInfoReducer';
 import { notifications } from './notificationsReducer';
 import { computation } from './computationReducer';
+import { websocket } from './webSocketReducer';
 
 export default combineReducers({
     datasetInfo,
     notifications,
     computation,
+    websocket,
 })
