9ae0ddff8efc561055a6d2298d9b35d9d1bbb7a5
Matteo Bogo <matteo.bogo@gmail.com>
2018-09-20 23:42:26 +0200

    Added ScrollLock for disabling page scrolling when the user interacts with the tile (for instance to increase/decrease zoom level of the heatmap). Added logic for mouse click, hover and wheel. For mouse click and hover added the computation of pixel coordinates within the tile. For mouse wheel added the logic for retrieving the delta of y-axis scrolling used to increase/decrease the heatmap zoom level

--- visualizer/client/src/components/StaticAnalysis/Tile.js
+++ visualizer/client/src/components/StaticAnalysis/Tile.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
 import FakeTile from '../../../public/images/something-went-wrong.svg';
 
+import * as localConstants from '../../utils/constants';
+
 import { LoadingOverlay, Loader } from 'react-overlay-loader';
 import 'react-overlay-loader/styles.css';
+
+import ScrollLock from 'react-scrolllock';
 
 export default class Tile extends Component {
 
@@ -14,9 +18,13 @@ export default class Tile extends Component {
 
             isLoading: false,
             isValid: false,
+            lockScroll: false,
         };
 
+        this.onMouseInteraction = this.onMouseInteraction.bind(this);
         this.onMouseClick = this.onMouseClick.bind(this);
+        this.onMouseHoover = this.onMouseHoover.bind(this);
+        this.onMouseWheel = this.onMouseWheel.bind(this);
     }
 
     componentDidMount() {
@@ -56,31 +64,52 @@ export default class Tile extends Component {
             });
     }
 
-    onMouseClick(event) {
+    onMouseInteraction(event, type) {
 
-        const { tileID, handleTileMouseClick } = this.props;
+        const { tileID, handleTileMouseInteraction } = this.props;
 
         //computes (x,y) pixels coords within the tile
         const bounds = event.target.getBoundingClientRect();
-        const pos_x = event.clientX - bounds.left;
-        const pos_y = event.clientY - bounds.top;
+        let pos_x = Math.round(event.clientX - bounds.left);
+        let pos_y = Math.round(event.clientY - bounds.top);
 
-        handleTileMouseClick({
-            tileX: tileID[0],
-            tileY: tileID[1],
-            imgX: pos_x,
-            imgY: pos_y,
+        pos_x = pos_x < 0 ? 0 : pos_x;
+        pos_y = pos_y < 0 ? 0 : pos_y;
+
+        handleTileMouseInteraction({
+            coordinates: {
+                tileX: tileID[0],
+                tileY: tileID[1],
+                imgX: pos_x,
+                imgY: pos_y,
+            },
+            type: type,
         });
+    }
+
+    onMouseClick(event) {
+
+        this.onMouseInteraction(event, localConstants._TYPE_MOUSE_CLICK);
     }
 
     onMouseHoover(event) {
 
+        this.onMouseInteraction(event, localConstants._TYPE_MOUSE_HOOVER);
+    }
 
+    onMouseWheel(event) {
+
+        const { handleTileMouseInteraction } = this.props;
+
+        handleTileMouseInteraction({
+            zoomTick: event.deltaY,
+            type: localConstants._TYPE_MOUSE_WHEEL,
+        });
     }
 
     render() {
 
-        const { isLoading, isValid } = this.state;
+        const { isLoading, isValid, lockScroll } = this.state;
         const { tileURL } = this.props;
 
         return (
@@ -89,8 +118,17 @@ export default class Tile extends Component {
                 <LoadingOverlay className="overlay-loader">
                     {
                         isValid ?
-                            <div onClick={(e) => this.onMouseClick(e)}>
+                            <div className="tile-container"
+                                 onClick={(e) => this.onMouseClick(e)}
+                                 onMouseMove={(e) => this.onMouseHoover(e)}
+                                 onWheel={(e) => this.onMouseWheel(e)}
+                                 onMouseEnter={() => this.setState({lockScroll: true})}
+                                 onMouseLeave={() => this.setState({lockScroll: false})}>
+
                                 <img className="tile" src={tileURL}/>
+                                {
+                                    lockScroll ? <ScrollLock/> : null
+                                }
                             </div>
                                 :
                             <div>
