d8f59aacdd145fa656cecd871fcc4f04ee2c36db
Matteo Bogo <matteo.bogo@gmail.com>
2018-09-20 16:31:56 +0200

    FakeTile is moved to an independent component and mounted by tile component when the URL with the png image of the tile is not available. Added logic for handling user mouse click and hoover (only skeleton). Refactored the tile URL validation logic.

--- visualizer/client/src/components/StaticAnalysis/Tile.js
+++ visualizer/client/src/components/StaticAnalysis/Tile.js
@@ -7,57 +7,96 @@ import 'react-overlay-loader/styles.css';
 export default class Tile extends Component {
 
     constructor() {
+
         super();
 
         this.state = {
 
-            imgSrc: FakeTile,
             isLoading: false,
+            isValid: false,
         };
+
+        this.onMouseClick = this.onMouseClick.bind(this);
     }
 
     componentDidMount() {
 
-
+        this.tileValidation();
     }
 
     componentDidUpdate(prevProps, prevState, prevContext) {
 
-        if (prevProps !== this.props) {
+        if (this.props.tileURL !== prevProps.tileURL) {
 
-            const { tileURL } = this.props;
-
-            //init (heatmap config not loaded)
-            if (tileURL === null) this.setState({imgSrc: FakeTile});
-            else {
-
-                this.setState({ isLoading: true });
-
-                fetch(tileURL)
-                    .then(res => {
-
-                        if (!res.ok || res.status !== 200) this.setState({imgSrc: FakeTile});
-                        else this.setState({imgSrc: tileURL});
-                    })
-                    .catch(err => {
-
-                        this.setState({imgSrc: tileURL});
-                    })
-                    .then(() => this.setState({ isLoading: false }));
-            }
+            this.tileValidation();
         }
+    }
+
+    tileValidation() {
+
+        this.setState({isLoading: true});
+
+        fetch(this.props.tileURL)
+            .then(res => {
+
+                let valid = false;
+                if (res.ok && res.status === 200) valid = true;
+
+                this.setState({
+                    isLoading: false,
+                    isValid: valid,
+                })
+            })
+            .catch(err => {
+
+                this.setState({
+                    isValid: false,
+                    isLoading: false,
+                });
+            });
+    }
+
+    onMouseClick(event) {
+
+        const { tileID, handleTileMouseClick } = this.props;
+
+        //computes (x,y) pixels coords within the tile
+        const bounds = event.target.getBoundingClientRect();
+        const pos_x = event.clientX - bounds.left;
+        const pos_y = event.clientY - bounds.top;
+
+        handleTileMouseClick({
+            tileX: tileID[0],
+            tileY: tileID[1],
+            imgX: pos_x,
+            imgY: pos_y,
+        });
+    }
+
+    onMouseHoover(event) {
+
 
     }
 
     render() {
 
-        const { imgSrc, isLoading } = this.state;
+        const { isLoading, isValid } = this.state;
+        const { tileURL } = this.props;
 
         return (
 
             <div className="tile-wrapper">
                 <LoadingOverlay className="overlay-loader">
-                    <img className="tile" src={imgSrc}/>
+                    {
+                        isValid ?
+                            <div onClick={(e) => this.onMouseClick(e)}>
+                                <img className="tile" src={tileURL}/>
+                            </div>
+                                :
+                            <div>
+                                <img className="tile" src={FakeTile}/>
+                            </div>
+                    }
                     <Loader loading={isLoading}/>
                 </LoadingOverlay>
             </div>
