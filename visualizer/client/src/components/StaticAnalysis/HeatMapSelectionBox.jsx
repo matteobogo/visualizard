4cfcce174ad874a890aeb6570e97c4d51b7b3591
Matteo Bogo <matteo.bogo@gmail.com>
2018-09-20 16:23:08 +0200

    Added stateless component for heatmap pixel selection: it shows the current timestamp and the current index of the machine selected

---
+++ visualizer/client/src/components/StaticAnalysis/HeatMapSelectionBox.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import PropTypes from 'prop-types';
+import { Clock, Server } from 'react-feather';
+import { ControlLabel } from 'react-bootstrap';
+
+import './HeatMapSelectionBox.css';
+
+export const HeatMapSelectionBox = (props) => {
+
+    return (
+        <div className="main-box-container">
+            <ControlLabel>{props.label}</ControlLabel>
+            <div className="properties-container">
+                <div className="icon-box">
+                    <Clock/>
+                </div>
+                <div className="property-box">
+                    <p>{props.timestamp}</p>
+                </div>
+                <div className="icon-box">
+                    <Server/>
+                </div>
+                <div className="property-box">
+                    <p>{props.machine}</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+HeatMapSelectionBox.propTypes = {
+    label: PropTypes.string.isRequired,
+    timestamp: PropTypes.string.isRequired,
+    machine: PropTypes.number.isRequired,
+};
