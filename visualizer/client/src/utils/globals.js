43acb081d691bb16639b07551134194fdb0036ce
Matteo Bogo <matteo.bogo@gmail.com>
2018-11-27 18:13:03 +0100

    moved the fun. for checking the existence of a specified object values in globals module

--- visualizer/client/src/utils/globals.js
+++ visualizer/client/src/utils/globals.js
@@ -1,1 +1,4 @@
 export const CleanData = (data) => data.map(v => v.toLowerCase().replace(/[_]/g, ' '));
+
+//check if all object's values are valid (not null/undefined/zero)
+export const checkValuesExistanceOfObject = (obj) => Object.values(obj).every((v) => v);
